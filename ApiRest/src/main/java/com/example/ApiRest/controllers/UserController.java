14873d71f7b514ad436d6b69842a53edff39d884
luis <lgarciiaruiiz@gmail.com>
2023-02-09 00:09:52 +0100

    Controller crear con responseEntity

--- ApiRest/src/main/java/com/example/ApiRest/controllers/UserController.java
+++ ApiRest/src/main/java/com/example/ApiRest/controllers/UserController.java
@@ -3,9 +3,14 @@ package com.example.ApiRest.controllers;
 import com.example.ApiRest.models.Usuario;
 import com.example.ApiRest.services.UsuarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -20,7 +25,18 @@ public class UserController {
     }
 
     @PostMapping("/usuario")
-    public void crear(@RequestBody Usuario usuario){
-    services.save(usuario);
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario){
+        Usuario usuarioNew=null;
+        Map<String,Object> response = new HashMap<>();
+        try{
+            usuarioNew = services.save(usuario);
+        }catch(DataAccessException e){
+            response.put("mensaje","Error al realizar el insert en la base de datos");
+            response.put("error",e.getMessage());
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","El usuario se a creado con exito");
+        response.put("usuario",usuarioNew);
+        return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
     }
 }
